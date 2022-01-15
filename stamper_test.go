de4b1157269b2a9a143519d3dbed0e6e57d8c246
Songmu <y.songmu@gmail.com>
2022-01-15 14:39:26 +0900

    introduce table testing

--- stamper_test.go
+++ stamper_test.go
@@ -12,119 +12,81 @@ import (
 )
 
 func TestStamper(t *testing.T) {
-	input := `Hello
-World!`
-	timeRegStr := `[0-9]{4}-[0-9]{2}-[0-9]{2}T` +
-		`[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}` +
-		`(?:Z|[-+][0-9]{2}:[0-9]{2}) `
-	reg := regexp.MustCompile(`\A` +
-		timeRegStr + "Hello\n" +
-		timeRegStr + `World!\z`)
+	testCases := []struct {
+		name    string
+		input   string
+		options []timestamper.Option
+		reg     *regexp.Regexp
+	}{{
+		name: "basic",
+		input: `Hello
+World!`,
+		reg: func() *regexp.Regexp {
+			timeRegStr := `[0-9]{4}-[0-9]{2}-[0-9]{2}T` +
+				`[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}` +
+				`(?:Z|[-+][0-9]{2}:[0-9]{2}) `
+			return regexp.MustCompile(`\A` +
+				timeRegStr + "Hello\n" +
+				timeRegStr + `World!\z`)
+		}(),
+	}, {
+		name: "utc",
+		input: `Hello
+World!`,
+		options: []timestamper.Option{timestamper.UTC()},
+		reg: func() *regexp.Regexp {
+			timeRegStr := `[0-9]{4}-[0-9]{2}-[0-9]{2}T` +
+				`[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}Z `
+			return regexp.MustCompile(`\A` +
+				timeRegStr + "Hello\n" +
+				timeRegStr + `World!\z`)
+		}(),
+	}, {
+		name: "layout",
+		input: `Hello
+World!`,
+		reg: func() *regexp.Regexp {
+			timeRegStr := `[0-9]{2}-[0-9]{2}-[0-9]{2} ` +
+				`[0-9]{2}:[0-9]{2}:[0-9]{2} `
+			return regexp.MustCompile(`\A` +
+				timeRegStr + "Hello\n" +
+				timeRegStr + `World!\z`)
+		}(),
+		options: []timestamper.Option{timestamper.Layout("06-01-02 15:04:05 ")},
+	}}
 
-	s := timestamper.New()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			input := tc.input
+			reg := tc.reg
+			s := timestamper.New(tc.options...)
 
-	t.Run("Reader", func(t *testing.T) {
-		r := transform.NewReader(strings.NewReader(input), s)
-		b, err := ioutil.ReadAll(r)
-		if err != nil {
-			t.Errorf("something went wrong: %s", err)
-		}
-		out := string(b)
-		if !reg.MatchString(out) {
-			t.Errorf("something went wrong. output: %s", out)
-		}
-		t.Logf("Output:\n%s", out)
-	})
+			t.Run("Reader", func(t *testing.T) {
+				r := transform.NewReader(strings.NewReader(input), s)
+				b, err := ioutil.ReadAll(r)
+				if err != nil {
+					t.Errorf("something went wrong: %s", err)
+				}
+				out := string(b)
+				if !reg.MatchString(out) {
+					t.Errorf("something went wrong. output: %s", out)
+				}
+				t.Logf("Output:\n%s", out)
+			})
 
-	s.Reset()
+			s.Reset()
 
-	t.Run("Writer", func(t *testing.T) {
-		buf := &bytes.Buffer{}
-		w := transform.NewWriter(buf, s)
-		w.Write([]byte(input))
-		w.Close()
-		out := buf.String()
-		if !reg.MatchString(out) {
-			t.Errorf("something went wrong. output: %s", out)
-		}
-		t.Logf("Output:\n%s", out)
-	})
-}
-
-func TestStamper_OptionUTC(t *testing.T) {
-	input := `Hello
-World!`
-	timeRegStr := `[0-9]{4}-[0-9]{2}-[0-9]{2}T` +
-		`[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}Z `
-	reg := regexp.MustCompile(`\A` +
-		timeRegStr + "Hello\n" +
-		timeRegStr + `World!\z`)
-
-	s := timestamper.New(timestamper.UTC())
-
-	t.Run("Reader", func(t *testing.T) {
-		r := transform.NewReader(strings.NewReader(input), s)
-		b, err := ioutil.ReadAll(r)
-		if err != nil {
-			t.Errorf("something went wrong: %s", err)
-		}
-		out := string(b)
-		if !reg.MatchString(out) {
-			t.Errorf("something went wrong. output: %s", out)
-		}
-		t.Logf("Output:\n%s", out)
-	})
-
-	s.Reset()
-
-	t.Run("Writer", func(t *testing.T) {
-		buf := &bytes.Buffer{}
-		w := transform.NewWriter(buf, s)
-		w.Write([]byte(input))
-		w.Close()
-		out := buf.String()
-		if !reg.MatchString(out) {
-			t.Errorf("something went wrong. output: %s", out)
-		}
-		t.Logf("Output:\n%s", out)
-	})
-}
-
-func TestStamper_OptionLayout(t *testing.T) {
-	input := `Hello
-World!`
-	timeRegStr := `[0-9]{2}-[0-9]{2}-[0-9]{2} ` +
-		`[0-9]{2}:[0-9]{2}:[0-9]{2} `
-	reg := regexp.MustCompile(`\A` +
-		timeRegStr + "Hello\n" +
-		timeRegStr + `World!\z`)
-
-	s := timestamper.New(timestamper.Layout("06-01-02 15:04:05 "))
-
-	t.Run("Reader", func(t *testing.T) {
-		r := transform.NewReader(strings.NewReader(input), s)
-		b, err := ioutil.ReadAll(r)
-		if err != nil {
-			t.Errorf("something went wrong: %s", err)
-		}
-		out := string(b)
-		if !reg.MatchString(out) {
-			t.Errorf("something went wrong. output: %s", out)
-		}
-		t.Logf("Output:\n%s", out)
-	})
-
-	s.Reset()
-
-	t.Run("Writer", func(t *testing.T) {
-		buf := &bytes.Buffer{}
-		w := transform.NewWriter(buf, s)
-		w.Write([]byte(input))
-		w.Close()
-		out := buf.String()
-		if !reg.MatchString(out) {
-			t.Errorf("something went wrong. output: %s", out)
-		}
-		t.Logf("Output:\n%s", out)
-	})
+			t.Run("Writer", func(t *testing.T) {
+				buf := &bytes.Buffer{}
+				w := transform.NewWriter(buf, s)
+				w.Write([]byte(input))
+				w.Close()
+				out := buf.String()
+				if !reg.MatchString(out) {
+					t.Errorf("something went wrong. output: %s", out)
+				}
+				t.Logf("Output:\n%s", out)
+			})
+		})
+	}
 }
